92b3c09592fc0beaad76dd6d9a51aef9321d225f
JIEUNI <jieundaeun@naver.com>
2024-11-18 14:38:50 +0900

    [init] #2 서버통신 예시 추가

--- app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/dummy/DummyRoute.kt
+++ app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/dummy/DummyRoute.kt
@@ -2,6 +2,7 @@ package org.andsopt.android.yes24ticket.presentation.ui.dummy
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -9,5 +10,9 @@ fun DummyRoute(
     paddingValues: PaddingValues,
     viewModel: DummyViewModel = hiltViewModel(),
 ) {
+    LaunchedEffect(Unit) {
+        viewModel.getDummyData()
+    }
+
     DummyScreen(paddingValues = paddingValues)
 }
