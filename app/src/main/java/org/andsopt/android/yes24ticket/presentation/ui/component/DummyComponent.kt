b33933d19b9b8b3a6c2b9565693827451b6eed71
JIEUNI <jieundaeun@naver.com>
2024-11-18 09:57:48 +0900

    [init] #2 Dummy Component 생성

---
+++ app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/component/DummyComponent.kt
@@ -0,0 +1,12 @@
+package org.andsopt.android.yes24ticket.presentation.ui.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DummyComponent (
+    modifier: Modifier = Modifier
+){
+    Text("string 추출 필수")
+}
