a07b02012901e2f925ba2e0739445f1ce8239742
JIEUNI <jieundaeun@naver.com>
2024-11-18 21:14:39 +0900

    [chore] #2 불필요한 인자 삭제

--- app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/navigator/component/BottomNavigationBar.kt
+++ app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/navigator/component/BottomNavigationBar.kt
@@ -1,6 +1,5 @@
 package org.andsopt.android.yes24ticket.presentation.ui.navigator.component
 
-import android.content.Context
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -8,7 +7,6 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,7 +16,6 @@ import org.andsopt.android.yes24ticket.ui.theme.YES24TICKETTheme
 @Composable
 fun BottomNavigationBar(
     modifier: Modifier = Modifier,
-    context: Context = LocalContext.current,
     isVisible: Boolean,
     navigationBarItems: List<BottomNavigationType>,
     currentNavigationBarItem: BottomNavigationType?,
@@ -33,7 +30,7 @@ fun BottomNavigationBar(
                     icon = {
                         Icon(
                             painter = painterResource(id = mainNavigationBarItemType.icon),
-                            contentDescription = context.getString(mainNavigationBarItemType.description),
+                            contentDescription = stringResource(mainNavigationBarItemType.description),
                         )
                     },
                     label = { Text(text = stringResource(mainNavigationBarItemType.description)) },
