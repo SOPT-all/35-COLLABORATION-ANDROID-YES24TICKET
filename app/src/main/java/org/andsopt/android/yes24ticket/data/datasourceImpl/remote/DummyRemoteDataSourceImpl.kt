f3a8645f473bd19b54ee5d7adcfea29714b83d80
JIEUNI <jieundaeun@naver.com>
2024-11-18 10:01:37 +0900

    [init] #2 Dummy Remote DataSourceImpl, RepositoryImpl 생성

---
+++ app/src/main/java/org/andsopt/android/yes24ticket/data/datasourceImpl/remote/DummyRemoteDataSourceImpl.kt
@@ -0,0 +1,16 @@
+package org.andsopt.android.yes24ticket.data.datasourceImpl.remote
+
+import org.andsopt.android.yes24ticket.data.datasource.remote.DummyRemoteDataSource
+import org.andsopt.android.yes24ticket.data.dto.request.RequestDummyDto
+import org.andsopt.android.yes24ticket.data.dto.response.ResponseDummyDto
+import org.andsopt.android.yes24ticket.data.service.DummyService
+import org.andsopt.android.yes24ticket.util.base.BaseResponse
+import javax.inject.Inject
+
+class DummyRemoteDataSourceImpl @Inject constructor(
+    private val dummyService: DummyService
+) : DummyRemoteDataSource {
+    override suspend fun getDummyData(requestDummyDto: RequestDummyDto): BaseResponse<ResponseDummyDto> =
+        dummyService.getDummyData(requestDummyDto)
+
+}
---
+++ app/src/main/java/org/andsopt/android/yes24ticket/data/repositoryImpl/DummyRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package org.andsopt.android.yes24ticket.data.repositoryImpl
+
+import org.andsopt.android.yes24ticket.data.datasource.remote.DummyRemoteDataSource
+import org.andsopt.android.yes24ticket.domain.model.DummyIdEntity
+import org.andsopt.android.yes24ticket.domain.model.DummyNameEntity
+import org.andsopt.android.yes24ticket.domain.repository.DummyRepository
+import javax.inject.Inject
+
+class DummyRepositoryImpl @Inject constructor(
+    private val dummyRemoteDataSource: DummyRemoteDataSource
+) : DummyRepository {
+    override suspend fun getDummyData(dummyNameEntity: DummyNameEntity): Result<DummyIdEntity> =
+        runCatching {
+            dummyRemoteDataSource.getDummyData(requestDummyDto = dummyNameEntity.toRequestDummyDto()).data.toDummyIdEntity()
+        }
+}
