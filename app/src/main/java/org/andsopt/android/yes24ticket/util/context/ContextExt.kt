2b220b127b0e0331b250899346f2e16035832ea1
JIEUNI <jieundaeun@naver.com>
2024-11-18 09:57:24 +0900

    [init] #2 Toast 확장함수 추가

---
+++ app/src/main/java/org/andsopt/android/yes24ticket/util/context/ContextExt.kt
@@ -0,0 +1,10 @@
+package org.andsopt.android.yes24ticket.util.context
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+fun Context.showToast(@StringRes message: Int, isShort: Boolean = true) {
+    val duration = if (isShort) Toast.LENGTH_SHORT else Toast.LENGTH_LONG
+    Toast.makeText(this, message, duration).show()
+}
