6dbcea56b8eb47032cfa7a3d76ec4544827e38e6
JIEUNI <jieundaeun@naver.com>
2024-11-18 10:02:32 +0900

    [init] #2 RetrofitModule 구현

---
+++ app/src/main/java/org/andsopt/android/yes24ticket/di/RetrofitModule.kt
@@ -0,0 +1,68 @@
+package org.andsopt.android.yes24ticket.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.andsopt.android.yes24ticket.BuildConfig
+import org.andsopt.android.yes24ticket.BuildConfig.DEBUG
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun providesJson(): Json = Json {
+        isLenient = true
+        prettyPrint = true
+        explicitNulls = false
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            connectTimeout(10, TimeUnit.SECONDS)
+            writeTimeout(10, TimeUnit.SECONDS)
+            readTimeout(10, TimeUnit.SECONDS)
+
+            addInterceptor { chain ->
+                val originalRequest = chain.request()
+                val requestBuilder = originalRequest.newBuilder()
+                chain.proceed(requestBuilder.build())
+            }
+
+            if (DEBUG) addInterceptor(loggingInterceptor)
+        }.build()
+
+    @Provides
+    @Singleton
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @ExperimentalSerializationApi
+    @Provides
+    @Singleton
+    fun providesYes24TicketRetrofit(okHttpClient: OkHttpClient, json: Json): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(
+                json.asConverterFactory(requireNotNull("application/json".toMediaTypeOrNull()))
+            )
+            .build()
+}
