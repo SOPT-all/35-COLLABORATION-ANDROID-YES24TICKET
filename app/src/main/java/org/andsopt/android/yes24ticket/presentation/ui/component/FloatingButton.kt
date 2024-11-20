5b23697bc79adbe7df5876b35a7549a1455d1e2c
beom84 <cjja00@konkuk.ac.kr>
2024-11-20 18:30:12 +0900

    [feat] #13 onClick 적용

--- app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/component/FloatingButton.kt
+++ app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/component/FloatingButton.kt
@@ -1,14 +1,10 @@
 package org.andsopt.android.yes24ticket.presentation.ui.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -16,10 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +30,15 @@ fun ScrollToTopFloatingButton(
         modifier = modifier
             .size(30.dp)
             .clip(RoundedCornerShape(2.dp))
+            .graphicsLayer {
+                shadowElevation = 4.dp.toPx() // 블러 반경에 해당
+                clip = true
+            }
             .background(Color.Red)
+            .padding(horizontal = 2.dp, vertical = 1.dp)
+            .clickable {
+                onClick()
+            }
            ,
         contentAlignment = Alignment.BottomCenter
 
