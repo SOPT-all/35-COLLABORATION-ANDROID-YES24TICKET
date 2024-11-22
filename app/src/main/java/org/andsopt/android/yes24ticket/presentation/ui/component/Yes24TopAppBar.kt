85dcf288f7a13e84a6c113670802c8cccdc71d8d
beom84 <cjja00@konkuk.ac.kr>
2024-11-22 19:30:30 +0900

    [chore] #23 file명 변경 , vector -> painter

--- app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/component/Yes24TopAppBar.kt
+++ app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/component/Yes24TopAppBar.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,16 +25,16 @@ import org.andsopt.android.yes24ticket.ui.theme.Yes24TicketTheme
 fun Yes24TopAppBar(modifier: Modifier = Modifier) {
     Row(
         modifier =
-            modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .background(Yes24TicketTheme.colorScheme.black)
-                .padding(vertical = 5.dp, horizontal = 14.dp),
+        modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(Yes24TicketTheme.colorScheme.black)
+            .padding(vertical = 5.dp, horizontal = 14.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         Image(
-            imageVector = ImageVector.vectorResource(R.drawable.img_logo_yes24_84_37),
+            painter = painterResource(R.drawable.img_logo_yes24_84_37),
             contentDescription = stringResource(R.string.top_app_bar_logo_description),
         )
 
