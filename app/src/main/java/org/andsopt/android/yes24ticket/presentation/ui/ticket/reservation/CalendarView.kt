9ee815a0c5198d8db22bfd89785255fd164aec23
Thirfir <sangbbang31@gmail.com>
2024-11-22 13:08:25 +0900

    [feat] #21 달력 컴포넌트

--- app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/ticket/reservation/CalendarView.kt
+++ app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/ticket/reservation/CalendarView.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
@@ -18,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.util.fastForEachIndexed
 import org.andsopt.android.yes24ticket.ui.theme.Yes24TicketTheme
 import java.time.DayOfWeek
 import java.time.LocalDate
@@ -27,6 +30,7 @@ import java.util.Locale
 @Composable
 fun CalendarView(
     selectableDates: List<LocalDate>,
+    days: List<List<Int>>,
     modifier: Modifier = Modifier,
 ) {
 
@@ -34,12 +38,35 @@ fun CalendarView(
         modifier = modifier.border(
             width = 1.dp,
             color = Yes24TicketTheme.colorScheme.gray150,
-            shape = RoundedCornerShape(2.dp)
-        ),
+            shape = RoundedCornerShape(4.dp)
+        ).background(
+            color = Yes24TicketTheme.colorScheme.white,
+        )
     ) {
         CalendarDayOfWeeksRow(
             modifier = Modifier.fillMaxWidth()
         )
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(),
+            thickness = 1.dp,
+            color = Yes24TicketTheme.colorScheme.gray150
+        )
+        days.fastForEach { weekDays ->
+            CalendarDaysRow(
+                days = weekDays,
+                selectableDates = selectableDates,
+                modifier = Modifier.fillMaxWidth().background(
+                    color = Yes24TicketTheme.colorScheme.white,
+                )
+            )
+            if (weekDays != days.last()) {
+                HorizontalDivider(
+                    modifier = Modifier.fillMaxWidth(),
+                    thickness = 1.dp,
+                    color = Yes24TicketTheme.colorScheme.gray150
+                )
+            }
+        }
     }
 }
 
@@ -58,24 +85,66 @@ private fun CalendarDayOfWeeksRow(
     Row(
         modifier = modifier.fillMaxWidth()
     ) {
-        dayOfWeeks.fastForEach {
+        dayOfWeeks.fastForEach { dayOfWeek ->
             Box (
-                modifier = Modifier.height(IntrinsicSize.Min).weight(1f).background(
-                    color = Yes24TicketTheme.colorScheme.gray100,
-                ),
+                modifier = Modifier
+                    .height(IntrinsicSize.Min)
+                    .weight(1f)
+                    .background(
+                        color = Yes24TicketTheme.colorScheme.gray100,
+                    ),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
                     modifier = Modifier.padding(vertical = 3.dp),
-                    text = it.getDisplayName(TextStyle.SHORT, Locale.KOREAN),
+                    text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN),
                     style = Yes24TicketTheme.typography.titleExtraBold12,
-                    color = when(it) {
+                    color = when(dayOfWeek) {
                         DayOfWeek.SUNDAY -> Yes24TicketTheme.colorScheme.red100
                         DayOfWeek.SATURDAY -> Yes24TicketTheme.colorScheme.blue200
                         else -> Yes24TicketTheme.colorScheme.gray700
                     }
                 )
-                if (it != dayOfWeeks.last()) {
+                if (dayOfWeek != dayOfWeeks.last()) {
+                    VerticalDivider(
+                        modifier = Modifier.align(Alignment.CenterEnd),
+                        thickness = 1.dp,
+                        color = Yes24TicketTheme.colorScheme.gray150
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CalendarDaysRow(
+    days: List<Int>,
+    selectableDates: List<LocalDate>,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        days.fastForEachIndexed { index, day ->
+            Box (
+                modifier = Modifier
+                    .height(IntrinsicSize.Min)
+                    .weight(1f)
+                    .aspectRatio(43f / 37f)
+                    .background(
+                        color = Yes24TicketTheme.colorScheme.white,
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                if (day != 0)
+                    Text(
+                        modifier = Modifier.padding(vertical = 6.dp),
+                        text = day.toString(),
+                        style = Yes24TicketTheme.typography.buttonSemiBold19,
+                        color = Yes24TicketTheme.colorScheme.gray900
+                    )
+                if (index != days.lastIndex) {
                     VerticalDivider(
                         modifier = Modifier.align(Alignment.CenterEnd),
                         thickness = 1.dp,
@@ -93,6 +162,13 @@ private fun CalendarViewPreview() {
     CalendarView(
         selectableDates = listOf(
             LocalDate.parse("2024-11-11"),
-        )
+        ),
+        days = listOf(0, 0, 0, 0, 0, 1, 2,
+            3, 4, 5, 6, 7, 8, 9,
+            10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23,
+            24, 25, 26, 27, 28, 29, 30,
+            31, 0, 0, 0, 0, 0, 0
+        ).chunked(7),
     )
 }
