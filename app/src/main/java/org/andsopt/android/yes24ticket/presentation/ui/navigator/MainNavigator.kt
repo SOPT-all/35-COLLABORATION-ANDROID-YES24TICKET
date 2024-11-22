a021dcab0172af5ea84e206654f0e47377d6c7f6
JIEUNI <jieundaeun@naver.com>
2024-11-23 01:53:59 +0900

    [chore] #22 Home navigator 패키지 구조 변경

--- app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/navigator/MainNavigator.kt
+++ app/src/main/java/org/andsopt/android/yes24ticket/presentation/ui/navigator/MainNavigator.kt
@@ -11,7 +11,7 @@ import androidx.navigation.navOptions
 import org.andsopt.android.yes24ticket.presentation.model.BottomNavigationRoute
 import org.andsopt.android.yes24ticket.presentation.model.Route
 import org.andsopt.android.yes24ticket.presentation.type.BottomNavigationType
-import org.andsopt.android.yes24ticket.presentation.ui.dummy.navigationHome
+import org.andsopt.android.yes24ticket.presentation.ui.home.navigationHome
 
 class MainNavigator(
     val navHostController: NavHostController,
