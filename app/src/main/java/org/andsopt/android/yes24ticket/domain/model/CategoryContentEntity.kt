f164dda0cc569eecd3d7dd183bdf4f1bde5b2445
JIEUNI <jieundaeun@naver.com>
2024-11-24 18:57:48 +0900

    [feat] #18 CategoryContentEntity 구현

---
+++ app/src/main/java/org/andsopt/android/yes24ticket/domain/model/CategoryContentEntity.kt
@@ -0,0 +1,9 @@
+package org.andsopt.android.yes24ticket.domain.model
+
+data class CategoryContentEntity(
+    val contentId: Long,
+    val contentImg: String,
+    val contentTitle: String,
+    val contentPlace: String,
+    val contentPeriod: String
+)
