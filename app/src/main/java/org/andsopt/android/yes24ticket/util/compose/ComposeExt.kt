3520fdb9a9f91b274e0912e728e359669bb6c370
JIEUNI <jieundaeun@naver.com>
2024-11-24 19:56:41 +0900

    [feat] #18 border 확장함수 추가

--- app/src/main/java/org/andsopt/android/yes24ticket/util/compose/ComposeExt.kt
+++ app/src/main/java/org/andsopt/android/yes24ticket/util/compose/ComposeExt.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 
 @Composable
@@ -22,3 +25,42 @@ fun Modifier.noRippleClickable(
         onClickLabel = onClickLabel,
         role = role,
     )
+
+fun Modifier.topBorder(
+    color: Color,
+    height: Float,
+) = this.drawWithContent {
+    drawContent()
+    drawLine(
+        color = color,
+        start = Offset(0f, 0f),
+        end = Offset(size.width, 0f),
+        strokeWidth = height,
+    )
+}
+
+fun Modifier.bottomBorder(
+    color: Color,
+    height: Float,
+) = this.drawWithContent {
+    drawContent()
+    drawLine(
+        color = color,
+        start = Offset(0f, size.height),
+        end = Offset(size.width, size.height),
+        strokeWidth = height,
+    )
+}
+
+fun Modifier.leftBorder(
+    color: Color,
+    width: Float,
+) = this.drawWithContent {
+    drawContent()
+    drawLine(
+        color = color,
+        start = Offset(0f, 0f),
+        end = Offset(0f, size.height),
+        strokeWidth = width,
+    )
+}
